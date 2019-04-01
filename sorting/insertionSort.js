8c5309df5ac4e76a372685ab1b2df08c6a985c6c
Sean Chavez <realseanchavez@gmail.com>
2019-04-01 15:23:19 -0700

    fix insertionSort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,11 +1,22 @@
-function insertionSort(nums) {
-  for (i = 0; i < nums.length; i++) {
-    for (j = i + 1; j < nums.length; j++) {
-      if (nums[j] < nums[i]) {
-        const temp = nums[j];
-        nums[j] = nums[i];
-        nums[i] = temp;
+const insertionSort = () => {
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] < nums[j]) {
+        let removed = nums.splice(i, 1);
+        nums.splice(j, 0, removed[0]);
       }
     }
   }
-}
+};
+
+// function insertionSort(nums) {
+//   for (i = 0; i < nums.length; i++) {
+//     for (j = i + 1; j < nums.length; j++) {
+//       if (nums[j] < nums[i]) {
+//         const temp = nums[j];
+//         nums[j] = nums[i];
+//         nums[i] = temp;
+//       }
+//     }
+//   }
+// }
