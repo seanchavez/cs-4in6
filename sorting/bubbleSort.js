307cc267a3b0922365a8b8aadcf72332e90bfc57
Sean Chavez <realseanchavez@gmail.com>
2019-04-01 11:22:54 -0700

    bubbleSort

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(nums) {
+  do {
+    var swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+}
