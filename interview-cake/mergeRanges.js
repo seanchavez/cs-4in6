0bcc14c7c5cbca8f2f5c72869fdad67f5743acbf
Sean Chavez <realseanchavez@gmail.com>
2019-04-02 21:36:12 -0700

    mergeRanges

---
+++ interview-cake/mergeRanges.js
@@ -0,0 +1,21 @@
+const clonedeep = require("lodash.clonedeep");
+
+const mergeRanges = meetings => {
+  meetingsClone = clonedeep(meetings);
+
+  const sorted = meetingsClone.sort((a, b) => a.startTime - b.startTime);
+
+  const merged = [sorted[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const current = sorted[i];
+    const lastMerged = merged[merged.length - 1];
+
+    if (current.startTime <= lastMerged.endTime) {
+      lastMerged.endTime = Math.max(lastMerged.endTime, current.endTime);
+    } else {
+      merged.push(current);
+    }
+  }
+  return merged;
+};
