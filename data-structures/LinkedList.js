c6dd7ff36e8edf01e18f76acce0bca4e84b28dd8
Sean Chavez <realseanchavez@gmail.com>
2019-04-04 21:41:39 -0700

    finish LinkedList

--- data-structures/LinkedList.js
+++ data-structures/LinkedList.js
@@ -36,10 +36,32 @@ class LinkedList {
   get(index) {
     let counter = 0;
     let current = this.head;
+
     while (counter <= index) {
       if (counter === index) {
         return current.value;
       }
+      current = current.next;
+      counter++;
+    }
+  }
+
+  delete(index) {
+    if (index === 0) {
+      const value = this.head.value;
+      this.head = this.head.next;
+      this.length--;
+      return value;
+    }
+    let counter = 1;
+    let current = this.head.next;
+    let prevNode = this.head;
+
+    while (counter <= index) {
+      if (counter === index) {
+        return current.value;
+      }
+      prevNode = current;
       current = current.next;
       counter++;
     }
