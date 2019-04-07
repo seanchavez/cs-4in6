22922b037b6717cfe2da50442dc28602f0708c8f
Sean Chavez <realseanchavez@gmail.com>
2019-04-06 21:22:26 -0700

    cheez

--- interview-cake/mergeRanges.js
+++ interview-cake/mergeRanges.js
@@ -1,6 +1,6 @@
 const clonedeep = require("lodash.clonedeep");
 
-function mergeRanges(meetings) {
+const mergeRanges = (meetings) => {
   meetingsClone = clonedeep(meetings);
 
   const sorted = meetingsClone.sort((a, b) => a.startTime - b.startTime);
