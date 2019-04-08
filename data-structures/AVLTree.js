52cfdc8233b1c3d4c0437b0cfb3c24206417a430
Sean Chavez <realseanchavez@gmail.com>
2019-04-07 17:42:09 -0700

    implement add method on Node

--- data-structures/AVLTree.js
+++ data-structures/AVLTree.js
@@ -1,27 +1,52 @@
 class Node {
   constructor(value = null, left = null, right = null) {
-    this.value = value
-    this.left = left
-    this.right = right
-    this.value = value
-    this.height = 1
+    this.value = value;
+    this.left = left;
+    this.right = right;
+    this.value = value;
+    this.height = 1;
   }
+
+  add(value) {
+    if (value < this.value) {
+      if (this.left) {
+        this.left.add(value);
+      } else {
+        this.left = new Node(value);
+      }
+      if (!this.right || this.right.height < this.left.height) {
+        this.height = this.left.height + 1;
+      }
+    } else {
+      if (this.right) {
+        this.right.add(value);
+      } else {
+        this.right = new Node(value);
+      }
+      if (!this.left || this.left.height < this.right.height) {
+        this.height = this.right.height + 1;
+      }
+    }
+    this.balance();
+  }
+
+  balance() {}
 }
 
 class Tree {
   constructor() {
-    this.root = null
+    this.root = null;
   }
 
   add(value) {
     if (!this.root) {
-      this.root = new Node(value)
+      this.root = new Node(value);
     } else {
-      this.root.add(value)
+      this.root.add(value);
     }
   }
 
   toObject() {
-    return this.root
+    return this.root;
   }
 }
